725f98386c17ced8fa0de2be8e0b087c6843f0b6
Jeff Manning <info@jeffmanning.ca>
2023-10-02 21:27:50 -0400

    finishing walking sprites and script
    Character can now roam around the game area

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,9 +1,28 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 30.0
+
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_key_pressed(KEY_LEFT) || Input.is_key_pressed(KEY_A):
 		_animated_sprite.play("walk_left")
+		global_position.x -= move_speed * delta
+		
+	elif Input.is_key_pressed(KEY_RIGHT) || Input.is_key_pressed(KEY_D):
+		_animated_sprite.play("walk_right")
+		global_position.x += move_speed * delta
+		
+	elif Input.is_key_pressed(KEY_DOWN) || Input.is_key_pressed(KEY_S):
+		_animated_sprite.play("walk_down")
+		global_position.y += move_speed * delta
+		
+	elif Input.is_key_pressed(KEY_UP) || Input.is_key_pressed(KEY_W):
+		_animated_sprite.play("walk_up")
+		global_position.y -= move_speed * delta
+		
 	else:
 		_animated_sprite.stop()
+		
+	print(velocity)
+	
